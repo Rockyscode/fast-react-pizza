f57645eef0fadc42e571602664d5a614c265debc
Rockyscode <anilsahu7377@gmail.com>
2023-11-24 00:00:51 +0530

    header button and input styled

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -5,7 +5,7 @@ import { createOrder } from "../../services/apiRestaurant";
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
   /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
-    str
+    str,
   );
 
 const fakeCart = [
@@ -50,9 +50,11 @@ export const action = async ({ request }) => {
   if (Object.keys(errors).length > 0) return errors;
 
   //if everything is okay then we create newOrder and redirect.
-  const newOrder = await createOrder(order);
-  console.log("In get Order ID", newOrder);
-  return redirect(`/order/${newOrder.id}`);
+  // const newOrder = await createOrder(order);
+  // console.log("In get Order ID", newOrder);
+  // return redirect(`/order/${newOrder.id}`);
+
+  return null;
 };
 
 function CreateOrder() {
@@ -87,7 +89,12 @@ function CreateOrder() {
         <div>
           <label>Address</label>
           <div>
-            <input type="text" name="address" required />
+            <input
+              type="text"
+              name="address"
+              required
+              className="w-full rounded-full border border-stone-200 px-4 py-2 text-sm transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-400 md:px-6 md:py-3"
+            />
           </div>
         </div>
 
@@ -98,13 +105,17 @@ function CreateOrder() {
             id="priority"
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
+            className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400 focus:ring-offset-2"
           />
           <label htmlFor="priority">Want to yo give your order priority?</label>
         </div>
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button
+            disabled={isSubmitting}
+            className="inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-slate-200 disabled:text-stone-400"
+          >
             {isSubmitting ? "Placing Order" : "Order now"}
           </button>
         </div>
--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Username() {
-  return <div className="hidden text-sm font-semibold">Rocky</div>;
+  return <div className="hidden text-sm font-semibold md:block">Rocky</div>;
 }
 
 export default Username;
